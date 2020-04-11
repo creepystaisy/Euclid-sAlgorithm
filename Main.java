77b2f08f336d347f1aebba8f6ef92033c1c3f44d
creepystaisy <49000256+creepystaisy@users.noreply.github.com>
2020-04-11 18:09:48 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static int Euclid(int a, int b)
     {
-        while (a != 0 && b !=0)
-        {
-            if(a > b)
-                a = a % b;
-            else
-                b = b % a;
+        while (b !=0) {
+            int tmp = a%b;
+            a = b;
+            b = tmp;
         }
         return a;
+    }
     }
     public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
